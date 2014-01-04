bded2d8a718c30aa93087184cd66e11d19ce3a40
Seb Jachec <sjachec1@gmail.com>
2014-01-04 13:29:19 +0000

    Added Sketch iOS UI template, with basic FramerJS code

---
+++ Resources/Framer/AppTemplate/app.js
@@ -0,0 +1,184 @@
+//// Utility functions ////
+
+isWebApp = function() {
+	return window.navigator.standalone;
+}
+
+setTimeSpeed = function(newSpeed) {
+	Framer.config.timeSpeedFactor = newSpeed
+}
+
+refreshStatusBarTime = function() {
+	currentDate = new Date();
+	hours = currentDate.getHours();
+	hours = hours % 12;
+	hours = hours ? hours : 12;
+	statusBarTime.html = hours+":"+(currentDate.getMinutes()<10?'0':'') + currentDate.getMinutes()+(currentDate.getHours()>=12? " PM" : " AM")
+}
+
+showAlert = function(title, message) {
+	if (utils.isMobile() && utils.isTouch()) {
+		alert(title+"\n\n"+message)
+	} else {
+		darkenScreen = new View({ x:0, y:0, height:1136, width: 640 })
+		darkenScreen.style =  {
+			"background-color": "rgba(0,0,0,0.25)",
+			"opacity": "0"
+		}
+
+		//Fade in
+		darkenScreen.animate({
+			properties:{ opacity:1 },
+			curve: "linear",
+			time: 100
+		})
+
+		alertBox = new View({ x:50, y:418, width:540, height:280 })
+		alertBox.superView = darkenScreen
+		alertBox.style = {
+		    "background-color": "rgba(255,255,255,0.85)",
+		    "border-radius": "14px",
+		    "color": "black",
+		    "font": "28px Helvetica Neue",
+		    "margin": "auto !important",
+		    "text-align": "center"
+		}
+
+		titleView = new View({ x:0, y:40, height:"auto", width:540 })
+		titleView.superView = alertBox
+		titleView.html = title
+		titleView.style = {
+			"color": "black",
+		    "font": "34px Helvetica Neue",
+		    "font-weight": "500",
+		    "text-align": "center"
+		}
+
+		messageView = new View({ x:0, y:104, height:"auto", width:"100%" })
+		messageView.superView = alertBox
+		messageView.html = message
+		messageView.style = {
+			"color": "black",
+		    "font": "28px Helvetica Neue",
+		    "font-weight": "300",
+		    "line-height": "44px",
+		    "margin-top": "-16px",
+		    "padding": "0 38px",
+		    "text-align": "center"
+		}
+
+		okButton = new View({ height:"auto", width:"100%"})
+		okButton.superView = alertBox
+		okButton.html = "OK"
+		okButton.style = {
+			"bottom": "0",
+			"color": "#007AFF",
+			"cursor": "pointer",
+			"font-size": "34px",
+			"font-weight": "500",
+			"margin-bottom": "32px",
+			"width": "100%"
+		}
+
+		okButton.on(pointerType, function() {
+			fadeAnimation = new Animation({
+				view:darkenScreen,
+				properties:{opacity:0},
+				curve: "linear",
+				time: 100
+			})
+			fadeAnimation.on("end", function() {
+				darkenScreen.destroy()
+			})
+			fadeAnimation.start()
+		})
+	}
+}
+
+
+//// Variables ////
+
+//Important pointer type variable (click or touch)
+pointerType = "click";
+
+//Possible values: linear, ease-in, ease-out, cubic-bezier(0.25,0,0.75,1), spring(100,50,250)
+animationCurve = "spring(100,20,1200)"
+
+statusBarTime = null
+
+
+//// Custom Functions, actions ////
+
+showSecondScreen = function() {
+	PSD.Second_Screen.animate({
+    	properties: { y: PSD.Status_Bar.height },
+    	curve: animationCurve
+  	})
+}
+
+hideSecondScreen = function() {
+	PSD.Second_Screen.animate({
+    	properties: { y: PSD.Main_Screen.height },
+    	curve: animationCurve
+  	})
+}
+
+PSD.Button.on(pointerType, function() {
+	showSecondScreen()
+})
+
+PSD.Back_Button.on(pointerType, function() {
+	hideSecondScreen()
+})
+
+PSD.First_Tab.on(pointerType, function() {
+	showAlert("First tab pressed", "Proin quis tortor orci. Etiam at risus et justo dignissim congue.")
+})
+
+
+start = function() {
+	//iPhone screen of right width to clip everything to
+	iPhoneScreen = new View({ x:0, y:0, width:640, height:1136 })
+	iPhoneScreen.style.overflow = "hidden"
+
+	PSD.Status_Bar.superView = iPhoneScreen
+	PSD.Main_Screen.superView = iPhoneScreen
+	PSD.Second_Screen.superView = iPhoneScreen
+
+	//Sort out hierarchy
+	PSD.Status_Bar.placeBefore(PSD.Main_Screen)
+	PSD.Second_Screen.placeBefore(PSD.Main_Screen)
+
+
+	if (utils.isTouch()) {
+		pointerType = "touchstart";
+	}
+
+
+	// Don't show status bar for web apps
+	if (isWebApp()) {
+		PSD.Status_Bar.opacity = 0
+	} else {
+		//PSD["Content"].y += 40
+	}
+
+
+	//Setup time in status bar
+	statusBarTime = new View({ x:268, y:4, width:115, height:34 })
+	statusBarTime.style = {
+	    "background-color": "transparent",
+	    "color": "black",
+	    "font": "26px Helvetica Neue",
+	    "text-align": "center"
+	}
+	refreshStatusBarTime()
+	utils.interval(1000,refreshStatusBarTime)
+
+
+	PSD.Second_Screen.x  = 0
+	PSD.Second_Screen.y = PSD.Main_Screen.height
+}
+
+
+//// Page load/app start ////
+start()
---
+++ Resources/Framer/README.md
@@ -0,0 +1,14 @@
+###About
+
+This is an iOS UI template that can be used with [FramerJS](http://www.framerjs.com), with Javascript code supplied for:
+
+* Layer hierarchy and positioning
+* Animating between the first and second screens, including a back button (vertical slide animation)
+* iOS alerts (created entirely in code, supports title and message)
+* Time that updates in the statusbar
+
+..and other small bits of useful code. 
+
+===
+
+Use this template with the [Sketch Framer plugin](https://github.com/bomberstudios/sketch-framer) (export your mockups to FramerJS).
